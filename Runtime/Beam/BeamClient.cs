11b38729bf70d3d7c40913836f7dfb62c724ed3f
lewinskimaciej <maciej@meritcircle.io>
2024-07-22 15:11:39 +0200

    fix: testnet url

--- Runtime/Beam/BeamClient.cs
+++ Runtime/Beam/BeamClient.cs
@@ -66,7 +66,7 @@ namespace Beam
                     apiUrl = "https://api.onbeam.com";
                     break;
                 default:
-                    apiUrl = "https://api.preview.onbeam.com";
+                    apiUrl = "https://api.testnet.onbeam.com";
                     break;
             }
 
